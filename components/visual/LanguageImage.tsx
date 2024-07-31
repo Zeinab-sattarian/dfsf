32d5df9fd6a42e4a59ca8d52548d46c6cff563c8
Zeinab-sattarian <you@example.com>
2024-07-31 10:58:18 +0330

    fix

--- components/visual/LanguageImage.tsx
+++ components/visual/LanguageImage.tsx
@@ -2,7 +2,5 @@ import { useLanguage } from "../../utils/lang";
 
 
 export default function LanguageImage(props){
-  let lang = useLanguage();
-  const src = `/img/i18n/${lang}/${props.src}`
-  return <img {...props} src={src} />
+
 }
--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-    localeDetection: false,
-  },
+
   async headers() {
     return [
       {
